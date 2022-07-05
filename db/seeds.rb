65a1da1bfb85f6094b4f03a59433f39338a3a86e
Alexis Parave <aparavev@gmail.com>
2022-07-04 23:12:20 -0500

    ADD: seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Start Seeding"
+
+Comment.destroy_all
+Photo.destroy_all
+Category.destroy_all
+
+puts "Seeding Categories"
+
+cat1 = Category.create(name: "Wallpapers", description: "From epic drone shots to inspiring moments in nature, find free HD wallpapers worthy of your screens.")
+cat2 = Category.create(name: "Nature", description: "Let's celebrate the magic of Mother Earth — with images of everything our planet has to offer.")
+cat3 = Category.create(name: "People", description: "Real people, captured. Photography has the power to reflect the world around us, give voice to individuals and groups.")
+
+
+puts "Seeding Photos"
+
+photo1 = Photo.create(title: "Purple lake", description: "Nice and big purple lake", category: cat2)
+photo2 = Photo.create(title: "Green plant", description: "Nice and big purple lake", category: cat2)
+photo3 = Photo.create(title: "Lonely bird", description: "Nice and big purple lake", category: cat2)
+
+puts "Seeding Comments"
+
+comment1 = Comment.create(content: "This is an example of a comment for this category.", commentable: cat2)
+comment2 = Comment.create(content: "This is an example of a comment for this category.", commentable: cat2)
+comment3 = Comment.create(content: "This is an example of a comment for this photo.", commentable: photo1)
+comment4 = Comment.create(content: "This is an example of a comment for this photo.", commentable: photo1)
+
+puts "End Seeding"
+

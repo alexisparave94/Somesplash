2dfc25897705cc8c3c3b537868b65ec5623bf861
Alexis Parave <aparavev@gmail.com>
2022-07-04 22:36:49 -0500

    ADD: validations to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,8 @@
 class Photo < ApplicationRecord
+  # Validations
+  validates :title, presence: true, uniqueness: true
+  validates :description, length: { in: 5..100 }
+
   # Associations
   belongs_to :category, counter_cache: true
   has_many :comments, as: :commentable, dependent: :destroy

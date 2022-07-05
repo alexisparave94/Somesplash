806cdf19ae1a4e4d6c05def2f21804c8d198976a
Alexis Parave <aparavev@gmail.com>
2022-07-04 23:11:09 -0500

    FIX: add default value to columns counts

--- db/migrate/20220705003748_create_photos.rb
+++ db/migrate/20220705003748_create_photos.rb
@@ -3,7 +3,7 @@ class CreatePhotos < ActiveRecord::Migration[7.0]
     create_table :photos do |t|
       t.string :title
       t.text :description
-      t.integer :comments_count
+      t.integer :comments_count, default: 0
       t.references :category, null: false, foreign_key: true
 
       t.timestamps

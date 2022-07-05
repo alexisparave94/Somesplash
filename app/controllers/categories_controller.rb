4fc1dceaa8439860bfc6cdb527da93353dda7584
Alexis Parave <aparavev@gmail.com>
2022-07-05 15:23:07 -0500

    ADD: view and methods to create a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,23 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
 end

fad754627572cc81dd7a19316e06ebd78822c7f4
Alexis Parave <aparavev@gmail.com>
2022-07-04 22:37:16 -0500

    ADD: validations to comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
+  # Validations
+  validates :content, length: { in: 5..100 }
+
   # Associations
   belongs_to :commentable, polymorphic: true, counter_cache: true
 end

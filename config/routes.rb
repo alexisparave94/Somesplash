66e51544b27a8309c95b4233932bb9f5cb2d3059
Alexis Parave <aparavev@gmail.com>
2022-07-05 14:33:47 -0500

    ADD: routes for categories views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "categories#index"
+  
+  # get 'categories', to: "categories#index"
+  resources "categories"
 end
